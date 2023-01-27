9067d6b272b876cd6d904844016c4c702cb4c663
admin <admin@YD05-03>
2023-01-27 17:56:28 +0900

    third

---
+++ Chapter03/src/com/yedam/operator/Operator.java
@@ -0,0 +1,12 @@
+package com.yedam.operator;
+
+public class Operator {
+
+	public static void main(String[] args) {		
+		//증감연산자
+		int a = 0;
+		System.out.println(a++);
+		System.out.println(++a);
+	}
+
+}
